62d2de47a448826f6056639591f0a90acf82a23d
Daniel Robbins <drobbins@funtoo.org>
2021-01-26 13:06:56 -0700

    Add WIP work on packaging subpop code.

---
+++ subpop/pkg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+"""
+The purpose of this file is to contain things related to packaging, such as location of plugin directory,
+and commands to manage the plugin repository for each python implementation.
+"""
