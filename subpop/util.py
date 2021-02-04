b83836f6a320d0fc58f6e2b0b86d17802ef2c29b
Daniel Robbins <drobbins@funtoo.org>
2021-02-03 22:50:18 -0700

    Working iteration over dynes!

--- subpop/util.py
+++ subpop/util.py
@@ -1,8 +1,11 @@
 import importlib
 import logging
 import os
+import pkgutil
 import sys
 import types
+from types import ModuleType
+
 import yaml
 
 
@@ -85,65 +88,22 @@ def _parse_subpop_yaml(self):
 			self._add_subsystem_from_yaml(subsystem_name, subsystem_dict)
 
 
-class PluginDirectory:
-	"""
-	TODO: needs updating
-
-	A ``PluginDirectory`` is the object that is put on the Hub when you use ``hub.add()``. So for example, if you
-	do ``hub.add("path/to/foo")``, then ``hub.foo`` will be a ``PluginDirectory`` object. This object handles references
-	to plugins that you want to access, such as ``hub.foo.bar``. By default, a Hub will run in "lazy" mode, which means
-	that except for the special ``init.py`` file, all plugins will only be loaded *on first reference* rather than
-	when your run ``hub.add()``. However, if you create your Hub with the ``lazy=False`` keyword argument, it will
-	call the ``PluginDirectory.load()`` method when adding a subsystem, which will automatically load all plugins
-	at startup. This is slower and not normally necessary so is not recommended.
-
-	The ``PluginDirectory.__getattr__`` method is what implements the 'hook' to dynamically load plugins as they are
-	referenced.
-	"""
-
-	def __init__(self, hub, path, model=None):
+class PluginDirectory(ModuleType):
+	def __init__(self, fullname, path=None, importer=None):
+		super().__init__(fullname)
 		self.path = path
-		self.hub = hub
-		self.loaded = False  # This means the plugin directory has been fully loaded and initialized.
-		self.plugins = {}
-		self.model = model
+		self.importer = importer
 
-	def ensure_plugin(self, plugin_name):
-		"""
-		This allows a plugin to be explicitly loaded, which is handy if you are using lazy loading (load on first
-		reference to something in a plugin) but your first interaction with it.
-		"""
-		if self.loaded:
-			if plugin_name not in self.plugins:
-				raise IndexError(f"Unable to find plugin {plugin_name}.")
-		else:
-			self.plugins[plugin_name] = self.hub.load_plugin(
-				os.path.join(self.path, plugin_name + ".py"), plugin_name, model=self.model
-			)
-
-	def load(self):
-		"""
-		The Hub will call this to force-load all of the plugins when running in non-lazy mode.
-		"""
-		for item in os.listdir(self.path):
-			if item in ["__init__.py", "init.py"]:
-				continue
-			if item.endswith(".py"):
-				plugin_name = item[:-3]
-				if plugin_name not in self.plugins:
-					self.plugins[plugin_name] = self.hub.load_plugin(os.path.join(self.path, item), plugin_name, model=self.model)
-		self.loaded = True
-
-	def __getattr__(self, plugin_name):
-		"""
-		This is the special method that will ensure a plugin is loaded and available when it is referenced on
-		the hub.
-		"""
-		if not self.loaded:
-			self.load()
-		if plugin_name not in self.plugins:
-			raise AttributeError(f"Plugin {plugin_name} not found.")
-		return self.plugins[plugin_name]
+	def __iter__(self):
+		if self.path is not None:
+			for file in os.listdir(self.path):
+				if file.endswith(".py"):
+					if file != "__init__.py":
+						modname = file[:-3]
+						try:
+							yield getattr(self, modname)
+						except AttributeError:
+							yield self.importer.load_module(self.__name__ + "." + modname)
 
 
 class SuperImporter:
@@ -172,14 +132,14 @@ class SuperImporter:
 
 	def load_module(self, fullname):
 		if fullname == self.prefix:
-			mod = sys.modules[fullname] = types.ModuleType(fullname)
+			mod = sys.modules[fullname] = PluginDirectory(fullname)
 			mod.__path__ = []
 		else:
 			slashname = "/".join(fullname.split(".")[1:])
 			fullpath = self.plugin_path + "/" + slashname
 			logging.warning(f"Searching in {fullpath}")
 			if os.path.isdir(fullpath):
-				mod = sys.modules[fullname] = types.ModuleType(fullname)
+				mod = sys.modules[fullname] = PluginDirectory(fullname, path=fullpath, importer=self)
 				mod.__path__ = []
 			else:
 				full_py = fullpath + ".py"
---
+++ tests/test_import_iter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import sys
+import types
+
+from subpop.util import SuperImporter
+
+
+def test_import_iter():
+	"""
+	Subpop has enhanced abilities related to discovering plugins in a subsystem. You can import a dyne using the
+	regular python "import" statement, and you're actually able to iterate over the plugins in the dyne!
+
+	This test makes sure it works properly. It tries to iterate over two plugins defined in the system dyne that
+	was imported.
+	"""
+
+	plugin_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plugin_test_dir")
+
+	if not hasattr(sys, "frozen"):
+		sys.meta_path.append(SuperImporter(plugin_path=plugin_dir))
+
+	import dyne.org.funtoo.powerbus.system as system
+
+	items = []
+	for plugin in system:
+		logging.warning(plugin)
+		items.append(plugin)
+	assert len(items) == 2
+	for item in items:
+		assert isinstance(item, types.ModuleType)
