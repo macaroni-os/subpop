f7809b9014b8d1e4cbcdf63e325a3ccaed0111ee
Daniel Robbins <drobbins@funtoo.org>
2022-01-30 12:49:36 -0700

    Bumping release to 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
 	name="subpop",
-	version="1.0.0",
+	version="2.0.0",
 	author="Daniel Robbins",
 	author_email="drobbins@funtoo.org",
 	description="A gentle evolution of the POP paradigm.",
